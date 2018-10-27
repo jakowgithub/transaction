01c1877a071b99940acc0a4f4668e5a8118dae01
galkin <oleksandr.galkin@intapp.com>
2018-10-27 10:59:50 +0300

    entities

---
+++ src/main/java/com/infopulse/entity/Order.java
@@ -0,0 +1,13 @@
+package com.infopulse.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Order {
+    Long id;
+    String orderName;
+}
