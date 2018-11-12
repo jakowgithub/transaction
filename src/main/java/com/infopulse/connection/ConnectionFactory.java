f87651b7d1ad9a979ba6d9fc6c344f511dcf615f
Sergij <jakowgit@gmail.com>
2018-11-12 22:26:06 +0200

    8 commit. Read/write success, stream, lambda. Connect DataSours pool HikariCP.

--- src/main/java/com/infopulse/connection/ConnectionFactory.java
+++ src/main/java/com/infopulse/connection/ConnectionFactory.java
@@ -1,23 +1,22 @@
 package com.infopulse.connection;
 
+import com.infopulse.exception.ConnectionException;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.DriverManager;
 
 public class ConnectionFactory {
     public static final ConnectionFactory factory = new ConnectionFactory();
-   // private Jdbc3PoolingDataSource source = new Jdbc3PoolingDataSource();
+    private HikariDataSource ds = new HikariDataSource();
     private String jdbcDB,localhost, databaseName,useSSL, user, password ;
 
 
     private ConnectionFactory() {
 
-//        source.setDataSourceName("A Data Source");
-//        source.setServerName("localhost");
-//        source.setDatabaseName("transaction");
-//        source.setUser("jakow");
-//        source.setPassword("pass1");
-//        source.setMaxConnections(10);
+        ds.setJdbcUrl("jdbc:postgresql://localhost:5432/transaction");
+        ds.setUsername("jakow");
+        ds.setPassword("pass1");
 
         jdbcDB="jdbc:postgresql://";
         localhost = "localhost/";
@@ -26,19 +25,14 @@ public class ConnectionFactory {
         user="user=jakow&";
         password = "password=pass1";
 
-        try {
-            Class.forName("org.postgresql.Driver");
-           // new InitialContext().rebind("DataSource", source);
-        } catch ( ClassNotFoundException e) { throw new RuntimeException(e); }
+       try { Class.forName("org.postgresql.Driver");
+           } catch ( ClassNotFoundException e) { throw new ConnectionException(e); }
     }
 
-    public static ConnectionFactory instance(){
-        return factory ;
-    }
+    public static ConnectionFactory instance(){ return factory ; }
 
     public synchronized Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(jdbcDB+ localhost+databaseName+useSSL+user+password);
-                //source.getConnection();
+        return ds.getConnection();
+             //DriverManager.getConnection(jdbcDB+ localhost+databaseName+useSSL+user+password);
     }
-
 }
